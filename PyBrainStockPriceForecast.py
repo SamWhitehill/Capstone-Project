b08517c2f89e86ca4dc47d7521b82bd25a9c1f15
Sam Whitehill <samwhitehill@ymail.com>
2016-12-04 22:06:47 -0500

    Change dataset sequence

--- PyBrainStockPriceForecast.py
+++ PyBrainStockPriceForecast.py
@@ -20,8 +20,10 @@ from sklearn.preprocessing import MinMaxScaler
 
 print ("Cite Reference: https://stavrossioutis.wordpress.com/2016/06/25/long-short-term-memory-rnns-for-stock-price-forecasting/")
 
-data = web.get_data_yahoo('SPY', dt.datetime(2015,1,22), dt.datetime(2016,06,22))
-data = data.ix[:,5].values.tolist()
+data = web.get_data_yahoo('SPY', dt.datetime(2015,10,22), dt.datetime(2016,06,22))
+#data = data.ix[:,5].values.tolist()
+
+data = data[['Open','High','Low','Adj Close','Volume']].values.tolist()
 
 # normalize the dataset
 
@@ -30,17 +32,30 @@ scaler =MinMaxScaler(feature_range=(0, 1)).fit(data)
 data = scaler.transform(data).tolist()
 
 #ds = SequentialDataSet(7, 1)
-ds = SequentialDataSet(6, 1)
 
-for i in range(0,len(data)-8):
+numDaysAhead =1
+numDaysLookBack=20
+numDim =5
+
+ds = SequentialDataSet(5, 1)
+
+for i in range(0,len(data)-(numDaysAhead+numDaysLookBack+1)):
     # training: previous 10 days and same day last year
-    sample = data[i:(i+6)] #+ [data[i-252]]
+    #sample = data[i:(i+6)] #+ [data[i-252]]
+    sample = data[i:(i+numDaysLookBack)]
     # target: 5 days ahead
-    target = data[i+7]
+    #target = data[i+10]
+    target =  [data[i+numDaysLookBack+numDaysAhead-1][3]] #adj close is 3rd item
+
+    #for each row of training data for this sample (window of trading days)
+    for input in sample:
+           
+        #need to add samples in a loop 
+        ds.appendLinked(input, target)
+
     ds.newSequence()
-    ds.addSample(sample, target)
 
-
+ds.removeSequence(i+1)
 
 #net = buildNetwork(7, 30, 1, hiddenclass=LSTMLayer, outputbias=False, recurrent=True)
 #reduced middle layer by Sam to 6, this must match number of past days used!
@@ -66,7 +81,7 @@ if False:
     n.sortModules()
 
 #BUILD RNN using SHORTCUT  
-net = buildNetwork(6, 8,1 , hiddenclass=LSTMLayer,  outputbias=False, recurrent=True, outclass=TanhLayer)
+net = buildNetwork(numDim,3,1 , hiddenclass=LSTMLayer,  outputbias=False, recurrent=True) # outclass=TanhLayer)
 
 trainer = RPropMinusTrainer(net, dataset=ds)
 
@@ -93,17 +108,21 @@ if False:
 
 target = np.zeros(ds.getNumSequences())
 prediction = np.zeros(ds.getNumSequences())
-    
+     
 i = 0
 
 for SeqCtr in range(ds.getNumSequences()):
-    for s, t in ds.getSequenceIterator(SeqCtr):
-        #target[i] = t
-        target[i] = scaler.inverse_transform(t)
-        prediction[i] = net.activate(s)
-        prediction[i] = scaler.inverse_transform([prediction[i]])
-
-        i += 1
+    input, target =ds.getSequence(SeqCtr)
+    #for s, t in ds.getSequenceIterator(SeqCtr):
+    #target[i] = t
+    #target[i] = scaler.inverse_transform(t[0]) #scaler.inverse_transform(t.tolist()*numDim)[3]
+    target[i] =scaler.inverse_transform([x[0] for x in target[0:numDim].tolist()])[3]
+    
+    #prediction[i] = net.activate(s)
+    prediction[i] = net.activate(input)
+    #prediction[i] = scaler.inverse_transform([prediction[i]]) 
+    prediction[i] = scaler.inverse_transform([prediction[i]]*numDim)[3]
+    i += 1
 
 plt.plot(target)
 plt.plot(prediction)
